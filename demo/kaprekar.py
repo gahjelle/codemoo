549bf915b72ea95a8cbd28225f0fdcfc3711f32c
Geir Arne Hjelle <geirarne@gmail.com>
2026-06-04 09:52:26 +0200

    Fix demo scripts

--- demo/kaprekar.py
+++ demo/kaprekar.py
@@ -5,7 +5,7 @@ Examples include (8 + 1)² = 81, (20 + 25)² = 2025, and (6048 + 1729)² = 60481
 
 import itertools
 
-LIMIT = 5_000
+LIMIT = 10_000
 
 for a, b in itertools.product(range(1, LIMIT), range(1, LIMIT)):
     n = a + b
